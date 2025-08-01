a700b54693119e9f696c47929e80fbe04c43827f
mukil10733 <kambojmukil341@gmail.com>
2025-08-01 16:24:43 +0530

    add c++ file

---
+++ 01_Basics/areaOfRectangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int l,b,area;
+    cout<<"enter l : ";
+    cin>>l;
+    cout<<"enter b : ";
+    cin>>b;
+    area=l*b;
+    cout<<area;
+    return 0;
+}
