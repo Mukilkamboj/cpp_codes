c0c5a2c533085539f440c1990b6ecaa8dba8b655
mukil10733 <kambojmukil341@gmail.com>
2025-08-02 14:43:40 +0530

    add file

---
+++ 03_Loops/factorialOf1stnNum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,fact=1;
+    cout<<"Enter the number : ";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        fact=fact*i;
+        i++;
+        cout<<fact<<endl;
+    }
+}
