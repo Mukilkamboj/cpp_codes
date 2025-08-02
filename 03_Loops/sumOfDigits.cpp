47c4d3fe730a27607308e3c4a6331eee5d147efd
mukil10733 <kambojmukil341@gmail.com>
2025-08-02 12:27:09 +0530

    add program

---
+++ 03_Loops/sumOfDigits.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rem,n,count=0;
+    cout<<"Enter the number : ";
+    cin>>n;
+    while(n!=0){
+        rem=n%10;
+        count=count+rem;
+        n=n/10;
+    }
+    cout<<count;
+}
